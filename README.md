a67963b9476efea515985778a48e13cdcc412d35
Gloria2507 <tuamiga_007@hotmail.com>
2017-10-10 12:24:52 -0500

    actualización

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cifrado César
+
+Este código nos sirve para poder cifrar y decifrar cualquier palabra, dando como respuesta el orden que tienen en el alfabeto según las reglas del cifrado César.
+
+Pseudocódigo.-
+Se emplea una funcion llamada "phrase". Que está dentro de la variable cipher.
+Luego añadimos una variable llamada tabla de valores, el cual empieza en 0, seguidamente se hara un recorrido con for, par aidentificas todos los elementos.
---
+++ js/app.js
@@ -0,0 +1,44 @@
+//Se crea la primera variable, llamda cifrado,que contiene la funcion frase//
+var cipher = function(phrase){
+//Creamos una variable llamada tabla de valores//
+var chartValue = 0;
+var cipherPhrase ='';
+//Aplicamos un for para recorrer cada letra que el usuario ha ingresado//
+     for (var i=0; i<phrase.length ; i++){
+       chartValue = phrase.charCodeAt(i);
+       if (chartValue>=65 && chartValue<=90){
+           chartValue = (chartValue - 65 + 33)%26+65;
+           chartValue = String.fromCharCode(chartValue);
+           cipherPhrase = cipherPhrase+ chartValue
+       }
+       else if(chartValue>=97 && chartValue<=122){
+           chartValue = (chartValue - 97 + 33)%26+97;
+           chartValue = String.fromCharCode(chartValue);
+           cipherPhrase = cipherPhrase+ chartValue
+       }
+     }
+return alert(cipherPhrase)
+}
+
+var deCipher = function(phrase2){
+var chartValue2 = 0;
+var deCipherPhrase ='';
+
+     for (var i=0; i<phrase2.length ; i++){
+       chartValue2 = phrase2.charCodeAt(i);
+       if (chartValue2>=65 && chartValue2<=90){
+           chartValue2 = (chartValue2 + 65 - 33)%26+65;
+           chartValue2 = String.fromCharCode(chartValue2);
+           deCipherPhrase = deCipherPhrase+ chartValue2
+       }
+       else if(chartValue2>=97 && chartValue2<=122){
+           chartValue2 = (chartValue2 + 97 - 45)%26+97;
+           chartValue2 = String.fromCharCode(chartValue2);
+           deCipherPhrase = deCipherPhrase+ chartValue2
+       }
+     }
+return alert(deCipherPhrase)
+}
+
+console.log(cipher(prompt("Escribe una frase")))
+console.log(deCipher(prompt("DECIFRADO")))
